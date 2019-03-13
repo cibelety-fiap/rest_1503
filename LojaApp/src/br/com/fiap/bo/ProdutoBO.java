1c6a8fa7e8f5fd2a63fb3b73e697a7bc4034fb7f
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-13 11:43:59 -0300

    Primeiro REST

---
+++ LojaApp/src/br/com/fiap/bo/ProdutoBO.java
@@ -0,0 +1,37 @@
+package br.com.fiap.bo;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fiap.to.ProdutoTO;
+
+public class ProdutoBO {
+	
+	public static ProdutoTO consultaBO(int codProd) {
+		
+		
+		if (codProd == 401) {
+			return new ProdutoTO(codProd,2.99,4, "Camisa Branca");
+		
+		} else if (codProd == 402) {
+			return new ProdutoTO(codProd,2.99,4, "Camisa Azul");
+			// System.out.println( p.getDescricao());
+		} else if (codProd == 403) {
+			return new ProdutoTO(codProd,2.99,4, "Camisa Rosa");
+		} else {
+			return new ProdutoTO(-1,0,0, "Produto não encontrado");
+		}
+	}
+
+	public List<ProdutoTO> listar() {
+		ArrayList<ProdutoTO> lista = new ArrayList<ProdutoTO>();
+		lista.add(ProdutoBO.consultaBO(401));
+		lista.add(ProdutoBO.consultaBO(402));
+		lista.add(ProdutoBO.consultaBO(403));
+		lista.add(ProdutoBO.consultaBO(404));
+		return lista;
+	}
+
+}
---
+++ LojaApp/src/br/com/fiap/resource/ProdutoResource.java
@@ -0,0 +1,25 @@
+package br.com.fiap.resource;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.fiap.bo.ProdutoBO;
+import br.com.fiap.to.ProdutoTO;
+
+@Path("/produto")
+public class ProdutoResource {
+	
+	private ProdutoBO produtoBo = new ProdutoBO();
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ProdutoTO> buscar1(){
+	
+		return produtoBo.listar();
+	}
+	
+
+}
